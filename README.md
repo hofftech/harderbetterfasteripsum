2920b22611db673eb82135c5ce5d5782f47758f1
Ken Hoff <ken@hoff.tech>
2016-04-07 11:27:45 -0600

    v3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # harderbetterfasteripsum
+
+scrolling to the bottom of the page generates more text
+on page load, generate text all the way to the bottom of the page
+page layout
--- app.js
+++ app.js
@@ -1,46 +1,32 @@
-ascii = "=,..............................................................................\n=,..............................................................................\n:,..............................................................................\n:,..............................................................................\n,,..............................................................................\n,,,.............................................................................\n,,,.............................................................................\n,,,.............................................................................\n,,,.............................................................................\n,,,.........................::.......................=..........................\n,,,.........................,...................................................\n,,,.............................................~:+?:...........................\n,,,.........................,=~~..............,:7?+77...I=:.==:.................\n,,,.........................?=7,.............~=7=,+,...?+I777777I=,.............\n,,,..................,.....?++..............:=7..........:7I,~,,,,,,,,..........\n,,,................=I7?=~,?+,....:==.~+7..,:=7~..:7....:+7:~,,.,.:..............\n,,,.............,II..,7I??+...~+,7+==?:..,~=+III7,....:I7,,,....................\n,,,...........:77I..777I?+...I?,.++++7,..~+77..::....=77,.......................\n,,,..........+77,..,~77?I,.,7I.I=,?++7I:=+7=,,.....,+7?=........................\n,,,..........,?77777=?7?~.77+,..,I+?++?=??7.......,+7,....,I,...................\n,,............,...,..,II..?7+,=?+.+7+,.7+7.......,+7=.....~77...................\n,,..............,.....,:,.,..,,.,:~.:.,?7,.:~...~+~,.....=77,..........,,.......\n,,,............,,~~,,,......,::,.....:I?,.........:....~+I7,......:+7,..........\n,..........,,.,:77I77=:.......,.....,,=...............,+I77,,=777:::,...........\n,,,.......:77:I7=.,77?+7..,.,..,,.,,,,,,,.,.,.,,~...~=?I777777=,,,,.............\n,,.......,77777=...77+,:.I7+,.,,,,=?.,,I+:.,,,~?7~.,7?I77777,:,,................\n,,.......,:777?~..,7II..=7+,..,,I+?7.,7?I:..=+I7,..,=?777777,,..................\n,,........,=777I?.,7=..77?,..,7I?I:,,7I+I.=+++I:..+II777?I777,..................\n,,........,7777??,7=...777:=7+~??,.,7I+++=,,7+II?I~?I7+,=,,+77,.................\n,,.......,777I777+:..:77II?~.=7+.,,7I+++.:.=7++I..7?7~+...,:=,?I,...............\n,,.......,77=.....,.:7I=:..:.7~.,.7+===,,..?==.:.:?7+,........:=,,..............\n,,.......,I77.,:~........,..,~?,.,,:~:=....~.,...7II?..............7............\n,........~7+,...............,,,...,:~,.........,7++:...............:7...........\n,........77~...................................7+,,................,,...........\n,........,~~..................................7=.,..............................\n,.......,....,I..............................:..~...............................\n,.......,.......................................................................\n,........................................:,.....................................\n,.........................................:.....................................\n................................................................................\n,...............................................................................\n................................................................................\n,...............................................................................\n,...............................................................................\n,...............................................................................\n,...............................................................................\n................................................................................\n................................................................................\n,,..............................................................................\n,,,,,..................................................................,,,,,,,,,"
-console.log(ascii);
+console.log(document.getElementById('pageBottom').scrollTop);
+
 // define all of our songs/lyrics
-var buttons = ["oh yeah", "one more time", "around the world", "harder", "better", "faster", "stronger", "make love", "give life back to music", "get lucky", "lose yourself to dance", "doin' it right", "human after all", "robot rock"]
-
-lyrics = [
-	"Da funk back to the punk, c'mon.",
-	"Around the world, around the world.",
-	// discovery lyrics
-	"one more time, we're gonna celebrate",
-	"oh yeah, all right, don't stop the dancing",
-	"harder better faster stronger",
-	// human after all songs
-	"Technologic. Technologic. Technologic. Technologic.",
-	"buy it use it break it fix it",
-	"trash it change it mail upgrade it",
-	// "charge it point it zoom it press it",
-	// "snap it work it quick erase it",
-	// "plug it play it burn it rip it",
-	// "drag and drop it zip unzip it",
-	// "lock it fill it curl it find it",
-	// "view it code it jam unlock it",
-	// "surf it, scroll it, pause it, click it",
-	// "cross it, crack it, switch update it",
-	// "name it, read it, tune it, print it",
-	// "scan it, send it, fax rename it",
-	// "touch it, bring it, pay it, watch it",
-	// "turn it, leave it, start format it",
-	"lose yourself to dance",
-	"come on come on come on come on come on, come on come on come on come on come on",
-	"robot rock",
-	"rollin' and scratchin'",
-	"face to face"
+songs = [
+	// albums
+	"homework", "discovery", "human after all", "random access memories",
+	// homework
+	"daftendirekt", "WDPK 83.7 FM", "revolution 909", "da funk", "phœnix", "fresh", "around the world", "rollin' & scratchin'", "teachers", "high fidelity", "rock'n roll", "oh yeah", "burnin'", "indo silver club", "alive", "funk",
+	// discovery
+	"one more time", "aerodynamic", "digital love", "harder, better, faster, stronger", "crescendolls", "nightvision", "superheroes", "high life", "something about us", "voyager", "veridis quo", "short circuit", "face to face", "too long",
+	// human after all
+	"human after all", "the prime time of your life", "robot rock", "steam machine", "make love", "the brainwasher", "on/off", "television rules the nation", "technologic", "emotion",
+	// random access memories
+	"give life back to music", "the game of love", "giorgio by moroder", "within", "instant crush", "lose yourself to dance", "touch", "get lucky", "beyond", "motherboard", "fragments of time", "doin' it right", "contact"
 ]
-
-
-
-
-// define our paragraph length, in words
-PARA_WORD_LENGTH = 150;
-$(".generateButton").text(buttons[Math.floor(Math.random() * buttons.length)])
+aroundTheWorld = ["around the world"]
+harderBetterFasterStronger = ["work it harder", "make it better", "do it faster", "makes us stronger", "more than ever", "hour after hour", "work is never over"]
+technologic = ["buy it, use it, break it, fix it", "trash it, change it, mail - upgrade it", "charge it, point it, zoom it, press it", "snap it, work it, quick - erase it", "write it, cut it, paste it, save it", "load it, check it, quick - rewrite it", "plug it, play it, burn it, rip it", "drag and drop it, zip - unzip it", "lock it, fill it, call it, find it", "view it, code it, jam - unlock it", "surf it, scroll it, pause it, click it", "cross it, crack it, switch - update it", "name it, read it, tune it, print it", "scan it, send it, fax - rename it", "touch it, bring it, pay it, watch it", "turn it, leave it, start - format it", "technologic. Technologic. Technologic. Technologic"]
+allWords = {
+		"songs": songs,
+		"aroundTheWorld": aroundTheWorld,
+		"harderBetterFasterStronger": harderBetterFasterStronger,
+		"technologic": technologic
+	}
+	// define our paragraph length, in words
+PARA_WORD_LENGTH = 100;
+// $(".generateButton").text(buttons[Math.floor(Math.random() * buttons.length)])
 var generate = function() {
-	$(".generateButton").text(buttons[Math.floor(Math.random() * buttons.length)])
+	// $(".generateButton").text(buttons[Math.floor(Math.random() * buttons.length)])
 	var paragraphCount = parseInt($(".paragraphInput").val())
 		// if paragraphInput does not contain a number
 		// , don't do anything.
@@ -53,12 +39,23 @@ var generate = function() {
 	}
 	// create the paragraphs for our ipsum
 	ipsum = []
-	for (var i = 0; i < paragraphCount; i++) {
-		// pick a song from an album to use as the lorem ipsum:
-		// var album = albums[Object.keys(albums)[Math.floor(Math.random() * Object.keys(albums).length)]]
-		// var song = album[Object.keys(album)[Math.floor(Math.random() * Object.keys(album).length)]]
-		ipsum[i] = generateParagraph(lyrics)
+		// get values of checkboxes
+	validKeys = []
+	$("input:checkbox").each(function(index, element) {
+		if (element.checked) {
+			validKeys.push(element.name)
+		}
+	})
+	filteredAllWords = {}
+	for (key of validKeys) {
+		filteredAllWords[key] = allWords[key]
 	}
+	for (var i = 0; i < paragraphCount; i++) {
+		// pick a set of words to use as the lorem ipsum:
+		var words = filteredAllWords[Object.keys(filteredAllWords)[Math.floor(Math.random() * Object.keys(filteredAllWords).length)]]
+		ipsum[i] = generateParagraph(words)
+	}
+	// clear existing ipsum text
 	$(".ipsum").html("")
 	for (var i = 0; i < ipsum.length; i++) {
 		$(".ipsum").append($('<p></p>').text(ipsum[i]))
@@ -79,7 +76,7 @@ var generateParagraph = function(lyrics) {
 		if (rand <= 0.5) {
 			newParagraphString = newParagraphString + " " + newParagraphLyrics[i]
 		} else if (rand <= 0.75) {
-			newParagraphString = newParagraphString + ", " + newParagraphLyrics[i].toLowerCase()
+			newParagraphString = newParagraphString + ", " + newParagraphLyrics[i]
 		} else {
 			newParagraphString = newParagraphString + ". " + newParagraphLyrics[i][0].toUpperCase() + newParagraphLyrics[i].slice(1)
 		}
@@ -88,13 +85,3 @@ var generateParagraph = function(lyrics) {
 	newParagraphString = newParagraphString.concat(".")
 	return newParagraphString
 }
-
-
-aroundTheWorld = ["around the world"]
-harderBetterFasterStronger = ["harder", "better", "faster", "stronger"]
-oneMoreTime = ["one more time", "we're gonna celebrate", "oh yeah, all right", "don't stop the dancing"]
-
-
-console.log(generateParagraph(aroundTheWorld));
-console.log(generateParagraph(harderBetterFasterStronger));
-console.log(generateParagraph(oneMoreTime));
